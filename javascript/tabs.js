7312cb09f458c7e22876fecc3d81465d5615f833
GreySilver <50336748+GreySilver@users.noreply.github.com>
2019-05-29 23:18:51 +0400

    Add files via upload

---
+++ javascript/tabs.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+
+	$('body').on('click','.tab-nav a',function(){
+	    $('.tab-nav a').removeClass('active');
+	    $(this).addClass('active');
+	    var href = $(this).attr('href');
+	    console.log(href)
+	    $('.tab-pane').removeClass('active').removeClass('in');
+	    $(href).addClass('active');
+	    setTimeout(function(){
+	      $(href).addClass('in');
+	    }, 200);
+	    return false;
+	  });
+
+});
